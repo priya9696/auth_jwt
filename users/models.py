dce6919d391d2a483084c144795ff8d5984692c0
Priyanka Deepak Walnekar <54662537+priya9696@users.noreply.github.com>
2021-06-25 17:32:24 +0530

    Add files via upload

---
+++ users/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    #confirm_password = models.CharField(max_length=255)
+    #address = models.TextField(max_length=300)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+
+
+
+
+
+
+
+
+
